2d8358f24435c1351831e8572fa24f7f0ffde1e2
Adones Balica <adonesbalica@gmail.com>
2022-04-08 15:45:23 -0300

    Requisição para api feita

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import consumTmdbApi from './consumTmdbApi';
 
 function App() {
+  useEffect(() => {
+    const loadAll = async () => {
+      const dadas = await consumTmdbApi.getHomeList();
+      console.log(dadas);
+    };
+    loadAll();
+  }, []);
+
   return (
     <div>
       <h1>oi</h1>
